1a3bb5646ee474f457c85df2b5a5b96eda419eff
Daniel Wallberg <daniel.wallberg@dawaco.se>
2023-02-09 14:46:02 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+# Importera Pandas
+import pandas as pd
+
+# Läs in CSV filen
+df = pd.read_csv('data/Automobile_data.csv')
+
+# Skapa en funktion som normaliserar datan i CSV filen
+def normalize(df):
+# Läs in CSV filen som en kopia
+    result = df.copy()
+# Välj alla kolumner som innehåller tal, exkludera index kolumnen
+    for feature_name in df[df.columns[~df.columns.isin(['index'])]].select_dtypes(include=['int', 'float']).columns:
+# Läs in värdena från dessa kolumner
+        max_value = df[feature_name].max()
+        min_value = df[feature_name].min()
+# Normalisera alla värden
+        result[feature_name] = (df[feature_name] - min_value) / (max_value - min_value)
+# Returnera resultatet
+    return result
+
+# Skapa en variabel som innehåller hela resultatet
+df2 = normalize(df)
+
+# Spara den nya normaliserade filen
+df2.to_csv('data/Automobile_data_normalized')
+
